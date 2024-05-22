c4d50d4d3a2095e516a2165844676d68b10e9b8e
Irina <irina.odinokova27@gmail.com>
2024-05-22 20:48:58 +1000

    my second commit

--- Python_lists_practice.py
+++ Python_lists_practice.py
@@ -1,3 +1,9 @@
 # This is my practice file to master Lists in Python
 # I am going to complete all excercises provided in Python Crash Course book
 
+fruit = ['apple', 'orange', 'banana', 'cherry']
+berry = ['strawberry', 'raspberry', 'boysenberry']
+
+my_fav_fruit = fruit.append('mandarin')
+print(my_fav_fruit)
+
