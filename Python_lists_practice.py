6f27c7b4eb81f41f8cce957abb00ffe7685e50ad
Irina <irina.odinokova27@gmail.com>
2024-05-23 19:13:13 +1000

    First 4 tasks

--- Python_lists_practice.py
+++ Python_lists_practice.py
@@ -18,3 +18,10 @@ wish_list = ['house', 'car', 'trip', 'furniture']
 
 for wish in wish_list:
     print(f'I would like to own a {wish}')
+
+# 3-4 Invite to dinner
+
+my_guests = ['mom', 'rob', 'leo']
+
+for guest in my_guests:
+    print(f'I"d like to invite you to dinner, {guest.title()}!')
