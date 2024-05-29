845416a3e5a7ab7d1e90822dee585cd4a6b94056
Irina <irina.odinokova27@gmail.com>
2024-05-29 14:53:42 +1000

    next 3 exercises

--- Dictionaries.py
+++ Dictionaries.py
@@ -36,3 +36,45 @@ glossary = {
 
 for term in glossary:
     print(f"The new word is: {term}. It means {glossary[term]}")
+
+# 6-4 Glossary 2
+
+glossary2 = {
+    "kurtosis":"weight of tails",
+    "skeweness":"assymetry",
+    "tiebreaker":"deciding identifier"
+}
+
+my_dict_words = glossary2.keys()
+my_dict_meanings = glossary2.values()
+
+glossary2["normality"] = "normal distribution"
+glossary2["mean"] = "average"
+
+print(my_dict_words)
+print(my_dict_meanings)
+
+# 6-5 River-Coutnry
+
+rivers = {
+    "volga" : "russia",
+    "nile" : "egypt",
+    "danube": "germany"
+}
+
+for river in rivers:
+    print(f"River {river.title()} flows through {rivers[river].title()}")
+
+# 6-6 Languages
+
+fav_language = {
+    "irina":"python",
+    "rob":"sas",
+    "dima":"C sharp",
+    "lena":"C sharp"
+}
+
+# print only unique languages
+
+for langg in set(fav_language.values()):
+    print(langg)
