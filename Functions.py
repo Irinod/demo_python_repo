9bca6eab69a36f8fc5eef49da24823f663e61aca
Irina <irina.odinokova27@gmail.com>
2024-06-01 13:58:55 +1000

    8-7 added

---
+++ Functions.py
@@ -0,0 +1,11 @@
+# 8-6. City Names: Write a function called city_country() that takes in the name
+# of a city and its country.
+
+def city_country(city_name, country_name):
+    '''This function combines city and country names'''
+    return f"{city_name} {country_name}".title()
+
+
+print(city_country("moscow","russia"))
+
+# 8-7 
