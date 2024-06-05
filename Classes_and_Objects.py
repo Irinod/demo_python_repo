8cfc4a554821d668baf87e4f39758fc76059907e
Irina <irina.odinokova27@gmail.com>
2024-06-05 11:33:55 +1000

    9-1 completed

--- Classes_and_Objects.py
+++ Classes_and_Objects.py
@@ -1,2 +1,31 @@
 # This is the program to practice Python classes and objects
 
+# 9-1 Restaurant
+
+class Restaurant():
+    '''Restaurant class'''
+    
+    def __init__(self,restaurant_name,cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    
+    def describe_restaurant(self):
+        print(f'{self.cuisine_type.title()} {self.restaurant_name.title()}')
+    
+    def open_restaurant(self):
+        print(f'The restaurant {self.restaurant_name.title()} is open.')
+
+
+# Creating an object from a Restaurant class:
+
+restaurant = Restaurant("Il patio", "italian")
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+# Calling object methods:
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+
