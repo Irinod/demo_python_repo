e3b992940d7886b47f8b1a3dd7ee836a418a7a52
Irina <irina.odinokova27@gmail.com>
2024-06-10 15:40:59 +1000

    added 10-4

--- Files_practice.py
+++ Files_practice.py
@@ -38,3 +38,26 @@ for line in lines:
 # 10-3. Guest: Write a program that prompts the user for their name. When they
 # respond, write their name to a file called guest.txt.
 
+file_path = 'C:/Users/irina/AppData/Local/Programs/Python/Python310/files/guests.txt'
+guest_name = input('Please, tell me your name: ')
+
+with open(file_path, 'w') as filename:
+    filename.write(guest_name)
+
+
+    
+# 10-4 Write a while loop that prompts users for their name. When
+# they enter their name, print a greeting to the screen and add a line recording
+# their visit in a file called guest_book.txt. Make sure each entry appears on a
+# new line in the file.
+
+file_path = 'C:/Users/irina/AppData/Local/Programs/Python/Python310/files/guests.txt'
+
+reply = 's'
+while reply != 'q':
+    reply = input('Please tell me your name: ')
+
+    with open(file_path,'a') as filename:
+        filename.write(reply+'\n')
+
+
